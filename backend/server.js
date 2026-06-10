d0124ce63ef1241f5cf90672acccdd44947aa61a
lupamo <arnoldlupamo@gmail.com>
2026-06-10 11:51:30 +0300

    add cors middleware for handling cross-origin requests

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 import express from 'express';
+import cors from 'cors';
 import bookRoutes from './routes/bookRoutes.js';
 import migrate from './database/schema.js';
 
 migrate();
+app.use(cors());
 
 const app = express();
 app.use(express.json());
