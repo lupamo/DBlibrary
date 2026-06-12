6a2f6137d851350c4c5b9632eccdd3bc49387106
lupamo <arnoldlupamo@gmail.com>
2026-06-12 13:54:33 +0300

    refactor server.js to improve structure and update API routes

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,21 @@
 import express from 'express';
 import cors from 'cors';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 import bookRoutes from './routes/bookRoutes.js';
 import migrate from './database/schema.js';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 migrate();
-app.use(cors());
 
 const app = express();
 app.use(express.json());
-app.use('/books', bookRoutes);
+app.use(cors());
+
+
+app.use(express.static(path.join(__dirname, '..', 'frontend')));
+app.use('/api/books', bookRoutes);
 
 app.listen(3000, () => console.log('Server running on port 3000'));
